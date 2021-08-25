bd13cdf83a03f11735763bbeb7b0b993870dba07
Richard Ambler <rambler@ibwya.net>
2021-08-25 20:58:31 +0800

    showing cycles

--- prisoners.py
+++ prisoners.py
@@ -1,58 +1,63 @@
 import random
 from sys import argv
 
-PRISONERS = 10
-GUESSES = 5
+prisoners = 10
+guesses = 5
 
-def simulate(verbose=True, method="random"):
+
+def simulate(verbose: bool = True, method: str = "random") -> int:
     """
-    Simulates [PRISONERS] prisoners searching for their cards given [GUESSES] guesses;
+    Simulates `prisoners` prisoners searching for their cards given `guesses` guesses;
     returns the number of prisoners who successfully find their cards.
 
-    If [verbose] is set to True, a description of the simulation will be printed out.
+    If `verbose` is set to True, a description of the simulation will be printed out.
 
-    If [method] is set to ["random"], prisoners will randomly pick a drawer they haven't already
-    searched for their next guess. If [method] is set to ["sequential"], prisoners will search the
+    If `method` is set to `"random"`, prisoners will randomly pick a drawer they haven't already
+    searched for their next guess. If `method` is set to `"sequential"`, prisoners will search the
     drawer that matches the card they have just found in the previously guessed drawer.
     """
 
-    def shuffled():
-        indices = [x + 1 for x in range(PRISONERS)]
+    def shuffled() -> list[int]:
+        """Returns shuffled prisoner indices."""
+        indices = [x + 1 for x in range(prisoners)]
         random.shuffle(indices)
         return indices
 
     cards = shuffled()
 
+    def show_cards() -> None:
+        print("Drawer:", " ".join([str(i + 1).rjust(3) for i in range(prisoners)]))
+        print("  Card:", " ".join([str(card).rjust(3) for card in cards]))
+
     if verbose:
         print("Card placement:\n")
-        print("Drawer\tCard")
-        print("\n".join(["{:<6}\t{}".format(i + 1, cards[i]) for i in range(PRISONERS)]))
+        show_cards()
 
     successes = 0
 
-    for prisoner in range(PRISONERS):
+    for prisoner in range(prisoners):
         if verbose:
-            print("\n  Prisoner {}'s turn...".format(prisoner + 1))
+            print(f"\n  Prisoner {prisoner + 1}'s turn...")
 
         choices = shuffled()
-        instrument_found = -1
+        card_found = -1
 
-        for guess in range(GUESSES):
-            box = choices[guess]
+        for guess in range(guesses):
+            drawer = choices[guess]
             if method == "sequential":
-                if instrument_found == -1:
-                    box = prisoner + 1
+                if card_found == -1:
+                    drawer = prisoner + 1
                 else:
-                    box = instrument_found
+                    drawer = card_found
 
-            instrument = cards[box - 1]
-            success = instrument == prisoner + 1
+            card = cards[drawer - 1]
+            success = card == prisoner + 1
 
             if verbose:
-                print("    Guess {}: Peeks in drawer {}...".format(guess + 1, box))
-                print("      Finds card {}.".format(instrument))
+                print(f"    Guess {guess + 1}: Peeks in drawer {drawer}...")
+                print(f"      Finds card {card}.")
 
-            instrument_found = instrument
+            card_found = card
             if success:
                 successes += 1
                 if verbose:
@@ -62,46 +67,80 @@ def simulate(verbose=True, method="random"):
             if verbose:
                 print("        Too bad!")
 
+    if method == "sequential" and verbose:
+        print()
+        show_cards()
+        cycles: list[list[int]] = []
+        for card in cards:
+            if any([card in ring for ring in cycles]):
+                continue
+            ring: list[int] = []
+            copy = card
+            while copy not in ring:
+                ring.append(copy)
+                copy = cards[copy - 1]
+            cycles.append(ring)
+        print(f"Cycles: {cycles}")
+
     return successes
 
-def perform_simulations(simulations, method):
-    print("\n{} simulations, {} searching:".format(simulations, method))
+
+def perform_simulations(simulations: int, method: str) -> None:
+    """
+    Performs `simulations` simulations with prisoners choosing the next door randomly
+    (`method="random"`) or sequentially (`method="sequential"`).
+    """
+    print(f"\n{simulations} simulations, {method} searching:")
     data = [simulate(verbose=False, method=method) for x in range(simulations)]
-    print("  Average number of prisoners finding their cards: {}".format(
-        sum(data) / simulations))
-    print("  Number of simulations in which all {} prisoners found their cards: {}".format(
-        PRISONERS, sum([data[i] == PRISONERS for i in range(simulations)])))
+    print(
+        f"  Average number of prisoners finding their cards: {sum(data) / simulations}"
+    )
+    print(
+        f"  Number of simulations in which all {prisoners} prisoners found their cards: {sum([data[i] == prisoners for i in range(simulations)])}"
+    )
     print("\nData:\n")
     per_row = 20
     for row in range(len(data) // per_row + 1):
-        print("\t".join(
-            ["{:<2}".format(data[e]) for e in range(
-                row * per_row, min([len(data), (row + 1) * per_row]))]))
+        print(
+            " ".join(
+                [
+                    f"{str(data[e]).rjust(4)}"
+                    for e in range(row * per_row, min([len(data), (row + 1) * per_row]))
+                ]
+            )
+        )
     print("\n")
 
-def example_simulation(method):
-    """
-    Produces a verbose description of a single simulation using method [method].
-    """
-    
-    print("Method: {}\n".format(method))
-    successes = simulate(method=method)
-    print("\nTotal successes: {}".format(successes))
 
-def show_help():
+def example_simulation(method: str) -> None:
+    """
+    Produces a verbose description of a single simulation using method `method`.
+    """
+
+    print(f"Method: {method}\n")
+    successes = simulate(method=method)
+    print(f"\nTotal successes: {successes}")
+
+
+def show_help() -> None:
     """
     Displays script instructions.
     """
     print("\nUse:")
-    print("  {0} --help        or {0} -h  \tDisplay help".format(argv[0]))
-    print("  {0} --random      or {0} -r  \tShow example simulation using random searching".format(argv[0]))
-    print("  {0} --random N    or {0} -r N\t(N some integer) Perform N simulations using random searching".format(argv[0]))
-    print("  {0} --sequence    or {0} -s  \tShow example simulation using sequential searching".format(argv[0]))
-    print("  {0} --sequence N  or {0} -s N\t(N some integer) Perform N simulations using sequential searching".format(argv[0]))
-    print("\n")
+    p = argv[0]
+    print(
+        f"""
+  {p} --help        or {p} -h  \tDisplay help
+  {p} --random      or {p} -r  \tShow example simulation using random searching
+  {p} --random N    or {p} -r N\t(N some integer) Perform N simulations using random searching
+  {p} --sequence    or {p} -s  \tShow example simulation using sequential searching
+  {p} --sequence N  or {p} -s N\t(N some integer) Perform N simulations using sequential searching
+"""
+    )
 
-def main():
-    print("Call: {}\n".format(" ".join(argv)))
+
+if __name__ == "__main__":
+    print(f"Call: {' '.join(argv)}\n")
     if len(argv) > 1:
         if argv[1] == "--help" or argv[1] == "-h":
             show_help()
@@ -127,5 +166,3 @@ def main():
             show_help()
     else:
         show_help()
-
-main()
